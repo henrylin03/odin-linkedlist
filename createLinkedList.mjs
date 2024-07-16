d228d0ce430a556ad8113cf028b9d6c06f1a59aa
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-16 15:27:42 +1000

    create new node in linkedlist's .append method

--- createLinkedList.mjs
+++ createLinkedList.mjs
@@ -1,5 +1,14 @@
-export default function createLinkedList() {
-  const linkedList = [];
+import createNode from "./createNode.mjs";
 
-  return { linkedList };
+export default function createLinkedList() {
+  const list = [];
+
+  // methods
+  const append = (value) => {
+    // add new node
+    const newNode = createNode();
+    // add new node to the end of list
+  };
+
+  return { list, append };
 }
