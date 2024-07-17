61009caf71c6e2803bf2adb9fcd775cd0386c261
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-17 13:19:09 +1000

    add all methods (incl those that are 'extra credit') for linked list, ready to go

--- createLinkedList.mjs
+++ createLinkedList.mjs
@@ -2,15 +2,64 @@ import createNode from "./createNode.mjs";
 
 export default function createLinkedList() {
   const list = [];
+  const size = list.length;
+  const head = ""; // returns first node in list
+  const tail = ""; // returns last node in list
 
-  // methods
+  // finder methods
+  const at = (idx) => {
+    // returns node at given index
+  };
+
+  const find = (value) => {
+    // retrns idx of node containing value, or null if not found
+  };
+
+  const contains = (value) => {
+    // returns true if value exists in linked list, otherwise returns false
+  };
+
+  // manipulation methods
   const append = (value) => {
-    // add new node
     const newNode = createNode();
     newNode.value = value;
 
-    // add new node to the end of list
+    // if the list is empty, the list is just the node
+
+    // get the last node in the list, and update it's .next pointer to the current node's value
   };
 
-  return { list, append };
+  const prepend = (value) => {
+    // add node with value to start of linked list
+  };
+
+  const pop = () => {
+    // removes last element of linked list
+  };
+
+  const toString = () => {
+    // print out based on format: ( value ) -> ( value ) => ( value ) -> null
+  };
+
+  const insertAt = (value, idx) => {
+    // inserts new node with provided value at given index
+  };
+
+  const removeAt = (idx) => {
+    // removes node at given idx
+  };
+
+  return {
+    list,
+    size,
+    head,
+    tail,
+    append,
+    prepend,
+    at,
+    pop,
+    find,
+    contains,
+    toString,
+  };
 }
