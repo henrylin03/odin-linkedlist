eafccb7a64fea6b475af6ba9d6c3817ae8d1d8f4
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-21 14:05:50 +1000

    write expected behaviour for logging to console in index.mjs for the tail() method

--- index.mjs
+++ index.mjs
@@ -3,10 +3,12 @@ import createLinkedList from "./createLinkedList.mjs";
 const linkedList = createLinkedList();
 
 // test
+console.log(linkedList.tail().value); // null
+
 linkedList.append(3);
 linkedList.append(4);
 linkedList.prepend(2);
 linkedList.prepend(1);
 
 console.log(linkedList.toString());
-// expected: ( 1 ) -> ( 2 ) -> ( 3 ) -> ( 4 ) -> null
+console.log(linkedList.tail().value); // expected: 4
