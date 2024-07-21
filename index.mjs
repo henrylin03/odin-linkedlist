2961909d7755c3356c47069a88218b9536174376
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-21 17:35:54 +1000

    add test cases

--- index.mjs
+++ index.mjs
@@ -3,15 +3,13 @@ import createLinkedList from "./createLinkedList.mjs";
 const linkedList = createLinkedList();
 
 // test
-linkedList.pop(); // expected: logged message
-
 linkedList.append(3);
-// linkedList.append(4);
-// linkedList.prepend(2);
-// linkedList.prepend(1);
+linkedList.append(4);
+linkedList.prepend(2);
+linkedList.prepend(1);
 
 console.log(linkedList.toString());
 
-linkedList.pop();
-
-console.log(linkedList.toString()); // expected: (1) -> (2) -> (3) -> null
+console.log(linkedList.find(3)) // expect: 2
+console.log(linkedList.find(1)) // expect: 0;
+console.log(linkedList.find(77)) // expect: null
