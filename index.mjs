01f179d2404422dfa147fe2436d83bcc1de7d8d2
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-21 18:45:12 +1000

    add test cases and confirm working. fixes #10

--- index.mjs
+++ index.mjs
@@ -3,8 +3,6 @@ import createLinkedList from "./createLinkedList.mjs";
 const linkedList = createLinkedList();
 
 // test
-console.log(linkedList.at(2)); // expect: null
-
 linkedList.append(3);
 linkedList.append(4);
 linkedList.prepend(2);
@@ -12,8 +10,5 @@ linkedList.prepend(1);
 
 console.log(linkedList.toString());
 
-console.log(linkedList.at(2).value); // expect: 3
-console.log(linkedList.at(0).value); // expect: 1
-console.log(linkedList.at(-1).value); // expect: 4
-console.log(linkedList.at(6)); // expect: null + error
-console.log(linkedList.at(-5)); // expect: null + error
+console.log(linkedList.contains(3)); // true
+console.log(linkedList.contains(7)); // false
