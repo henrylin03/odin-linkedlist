fffa05fdf8e7858729baa6b0d2094be539c937a3
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-21 14:15:47 +1000

    add test cases in index.mjs

--- index.mjs
+++ index.mjs
@@ -3,12 +3,15 @@ import createLinkedList from "./createLinkedList.mjs";
 const linkedList = createLinkedList();
 
 // test
-console.log(linkedList.tail()); // null
+console.log(linkedList.size()); // expected: 0
 
 linkedList.append(3);
+console.log(linkedList.size()); // expected: 1
 linkedList.append(4);
+console.log(linkedList.size()); // expected: 2
 linkedList.prepend(2);
+console.log(linkedList.size()); // expected: 3
 linkedList.prepend(1);
+console.log(linkedList.size()); // expected: 4
 
 console.log(linkedList.toString());
-console.log(linkedList.tail().value); // expected: 4
