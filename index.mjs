6bd291bed02d6edf49bb8d1bcf1218e6195ebb95
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-21 13:24:26 +1000

    add test cases and also call toString to check if actually #2 was done properly
    issue #2 was about appending and prepending nodes. we are testing those methods here in index.mjs

--- index.mjs
+++ index.mjs
@@ -1,4 +1,12 @@
 import createLinkedList from "./createLinkedList.mjs";
 
 const linkedList = createLinkedList();
-linkedList.append("test");
+
+// test
+linkedList.append(3);
+linkedList.append(4);
+linkedList.prepend(1);
+linkedList.prepend(2);
+
+console.log(linkedList.toString());
+// expected: ( 1 ) -> ( 2 ) -> ( 3 ) -> ( 4 ) -> null
