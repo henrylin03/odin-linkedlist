6a0520be53422baed79601477cbfde47536013fd
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-21 14:24:26 +1000

    add test case in index.mjs

--- index.mjs
+++ index.mjs
@@ -3,15 +3,13 @@ import createLinkedList from "./createLinkedList.mjs";
 const linkedList = createLinkedList();
 
 // test
-console.log(linkedList.size()); // expected: 0
-
 linkedList.append(3);
-console.log(linkedList.size()); // expected: 1
 linkedList.append(4);
-console.log(linkedList.size()); // expected: 2
 linkedList.prepend(2);
-console.log(linkedList.size()); // expected: 3
 linkedList.prepend(1);
-console.log(linkedList.size()); // expected: 4
 
 console.log(linkedList.toString());
+
+linkedList.pop();
+
+console.log(linkedList.toString()); // expected: (1) -> (2) -> (3) -> null
